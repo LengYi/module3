b9c3557a5984d7f2b482ce98825d570dcd9517fc
戴亮金 <dailiangjin@vv.cn>
2020-08-08 16:35:47 +0800

    【Release】v0.1.2

---
+++ Release_ios/Classes/modul3ViewController.h
@@ -0,0 +1,17 @@
+//
+//  RnLoaderManager.h
+//  AwesomeProject
+//
+//  Created by ice on 2020/7/14.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface modul3ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
