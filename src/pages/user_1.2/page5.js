7c61e171092ef45a7ae52e92bc99a21ac516ddd3
戴亮金 <dailiangjin@vv.cn>
2020-08-08 15:47:37 +0800

    安装新页面

---
+++ src/pages/user_1.2/page5.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+class Page5 extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <SafeAreaView>
+        <Text>page5</Text>
+      </SafeAreaView>
+      </View>
+    );
+  }
+}
+
+export default Page5;
